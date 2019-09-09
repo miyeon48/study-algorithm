bc8151993da50feb97f1d3bb5925b635bbdc193d
신미연 <miyeon4815@naver.com>
2019-09-10 00:30:43 +0900

    update

--- src/study/dp/MakeOne.java
+++ src/study/dp/MakeOne.java
@@ -23,6 +23,7 @@ public class MakeOne {
 		array[1] = 0;
 		
 		for(int i = 2; i < array.length; i++ ){
+			array[i] = array[i-1] + 1;
 			if(i % 3 == 0) 
 				array[i] = Math.min(array[i], array[i/3] + 1); 
 			if(i % 2 == 0) 
